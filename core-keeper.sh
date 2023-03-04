fe3f77bbb760d28c22814ff78100d46ad1d4b3fa
akuma548 <akuma548@hotmail.com>
2023-03-03 22:29:43 -0800

    Update core-keeper.sh

--- core-keeper.sh
+++ core-keeper.sh
@@ -3,22 +3,6 @@ sudo apt install unzip apt-transport-https ca-certificates curl gnupg lsb-releas
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 sudo apt update
 
-#install AWS CLI
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
-sudo ./aws/install
-
-#create random string for password
-CKPW=$(echo $RANDOM | md5sum | head -c 20)
-
-#get stackname created by user data script and update SSM parameter name with this to make it unique
-STACKNAME=$(</tmp/mcParamName.txt)
-PARAMNAME=mcCoreKeeperPW-$STACKNAME
-
-#put random string into parameter store as encrypted string value
-aws ssm put-parameter --name $PARAMNAME --value $CKPW --type "SecureString" --overwrite
-
-
 #install docker and core keeper app on docker
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo apt install docker-compose -y
